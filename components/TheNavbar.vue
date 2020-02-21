3effa1f152e83bdc2d94b54fb255b9d05c6af8a4
ewen-lbh <ewen.lebihan7@gmail.com>
2020-02-21 17:29:52 +0100

    📱 <TheNavbar> Adapt navbar for mobile screens

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,21 +1,27 @@
 <template lang="pug">
-.nav-wrapper(:class="{ scrolled }")
+//-TODO: Animate in/out of nav items (back&forth between creations/about/contact & ← go back, ewen le bihan to breadcrumbs)
+//-TODO: Animate (swoosh-like motion) current page indicator bar (eg. from CREATIONS to CONTACT), stretch to both then stretch back to dest.
+.nav-wrapper(:class="{ scrolled, opened }")
   nav
+    // Left
     template(v-if="backURL")
       NavItem.go-back(:to="backURL"): Iconed(icon="arrow-left") {{ $t('back') }}
     template(v-else)
       NavItem(to="/") {{ $t('works') }}
       NavItem(to="/about") {{ $t('about') }}
       NavItem(to="/contact") {{ $t('contact') }}
+    // Center
 
     span.center.breadcrumbs(v-if="breadcrumbs.length")
       NavItem.breadcrumb(
         v-for="fragment in breadcrumbs" :key="fragment"
         :to="fragment"
+        no-line
       ) {{ fragment }}
     span.center(v-else)
       NavItem Ewen Le Bihan
-
+    NavItem.switch-menu.center(@click="switchMenu") {{ $t(switchMenuText) }}
+    // Right
     NavLanguageSwitch.right
 </template>
 
@@ -28,10 +34,14 @@ export default {
   components: { NavItem, NavLanguageSwitch, Iconed },
   data() {
     return {
-      scrolled: false
+      scrolled: false,
+      opened: false
     }
   },
   computed: {
+    switchMenuText() {
+      return this.opened ? 'closeMenu' : 'openMenu'
+    },
     backURL() {
       const path = this.$route.path.replace('fr/', '')
       const fragments = path.split('/')
@@ -68,6 +78,16 @@ export default {
         this.scrolled = false
       }
     })
+  },
+  methods: {
+    switchMenu() {
+      this.opened = !this.opened
+      if (this.opened) {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = 'auto'
+      }
+    }
   }
 }
 </script>
@@ -82,27 +102,73 @@ export default {
   align-items center
   justify-content center
   top 0
+  box-sizing border-box
+  transition all 0.5s ease, border 0.25s ease
+  border 2px solid transparent
+  z-index: 1000
 
 nav
   width 100%
   padding 0 20px
   display flex
   align-items center
-  .--nav-item:not(:last-child)
-    margin-right 1.5em
-  .right
-    margin-left auto
-  .center
-    margin 0 auto
-  .center.breadcrumbs
-    position fixed
-    left 50%
-    transform translateX(-50%)
-  .breadcrumb
-    margin-right: 0 !important
-  .breadcrumb:not(:last-child)::after
-    content '/'
-    opacity: 0.25
-  .go-back
-    border none
+
+.--nav-item:not(:last-child)
+  margin-right 1.5em
+.right
+  margin-left auto
+.center
+  margin 0 auto
+.center.breadcrumbs
+  position fixed
+  left 50%
+  transform translateX(-50%)
+.breadcrumb
+  margin-right: 0 !important
+.breadcrumb:not(:last-child)::after
+  content '/'
+  opacity: 0.25
+.go-back
+  border none
+.switch-menu
+  display none
+  position fixed
+  bottom: 16px
+  left 50%
+  transform translateX(-50%)
+  z-index: 1010
+
+@media (max-width 900px)
+  .center:not(.switch-menu)
+    display none
+
+@media (max-width 600px)
+  .nav-wrapper
+    bottom: 0
+    top unset
+    border-top-color black
+  .switch-menu
+    display inline-block
+  .--nav-item:not(.switch-menu)
+    display none
+
+.opened
+  bottom: 0
+  left: 0
+  right: 0
+  z-index: 1000
+  height 100vh
+  border-color black
+  align-items flex-end
+  nav
+    flex-direction column
+    justify-content flex-end
+    align-items flex-start
+    margin-bottom: 100px
+  .--nav-item
+    display block
+    margin-right: 0
+    margin-left: 0
+  .--nav-item:not(:last-child):not(.switch-menu)
+    margin-bottom .5em
 </style>
