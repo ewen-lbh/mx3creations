cc06d1063e7181e86b9db859d76c73f6be98d2b7
ewen-lbh <ewen.lebihan7@gmail.com>
2020-02-26 19:43:28 +0100

    💄 /:collection : Make <Gallery> take the whole page width

--- pages/_collection/index.vue
+++ pages/_collection/index.vue
@@ -29,11 +29,16 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+main
+  width 100%
+
 h1
   font-size 10vmin //TODO: Scale down to fit text length, target max fontsize=80px
   font-family Work Sans, sans-serif
   letter-spacing: -0.025em
   // margin-top: -6vmin
   line-height: 0.8
-  // color var(--bgi)
+
+.--gallery
+  margin: 0 auto
 </style>
