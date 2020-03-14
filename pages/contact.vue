1e28369d860ac732c61c954d6f533d050eed3da3
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-14 16:18:32 +0100

    🚧 Start the contact page

---
+++ pages/contact.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+main
+  h1 Me contacter
+  section: form(name="contact" netlify)
+    p: label 
+      | Votre e-mail
+      input(type="email" name="email")
+    p: label
+      | Votre message
+      textarea(rows="20" cols="40")
+    
+    p: button(type="submit") Envoyer
+</template>
+
+<style lang="stylus" scoped>
+form
+  display flex
+  justify-content center
+  flex-direction column
+  align-items center
+  width 100%
+  label, p
+    display flex
+    align-items center
+    flex-direction column
+    text-align center
+  p, label, input, textarea
+    width 100%
+</style>
