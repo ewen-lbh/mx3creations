1865363d94c036f057af1c2a09c02e35e24ac15d
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-14 16:20:42 +0100

    🔎 Add a title template

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ export default {
    */
   head: {
     title: "Ewen's works.",
+    titleTemplate: "%s · Ewen Le Bihan",
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
