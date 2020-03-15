d0d02f45b6336bf5db82ad3d4e4b23f5136e4b91
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-15 16:24:27 +0100

    🚸 <Gallery> Show title & description on images

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -2,15 +2,22 @@
 .--gallery
   ul
     template(v-for="(work, i) in works")
-      li(v-if="getWorkFrontSrc(work)" :key="i")
+      li(:key="i")
         nuxt-link(:to="getWorkDetailsHref(work)")
+          .titles-layer(:style="getWorkTitlesLayerStyles(work)")
+            h4.collection(v-if="work.collection") {{ work.collection.name }}
+            h3.name {{ work.name }}
+            p.description(v-html="work.description || work.collection.description")
           img(
             :src="getWorkFrontSrc(work)"
+            :srcset="getWorkFrontSrcSet(work)"
             :title="work.name"
           )
 </template>
 
 <script>
+import tinycolor from 'tinycolor2'
+
 export default {
   props: {
     works: {
@@ -30,42 +37,99 @@ export default {
   },
   methods: {
     getWorkFrontSrc(work) {
-      return 'https://placehold.it/500/500'
       // eslint-disable-next-line no-unreachable
       let src = '/works/'
-      if (work.front === null) return null
+      if (work.front === null) return 'https://placehold.it/500/500'
       src += work.directory + '/' + work.front
       return src
+    },
+    getWorkFrontSrcSet(work) {
+      const thumbsDir = work.directory + '/thumbs/'
+      const srcset = {
+        '1x': this.getWorkFrontSrc(work),
+        '20w': thumbsDir + '20.png',
+        '150w': thumbsDir + '150.png',
+        '500w': thumbsDir + '500.png'
+      }
+      let srcsetString = ''
+      Object.entries(srcset).forEach(([sizeDescriptor, src]) => {
+        srcsetString += src + ' ' + sizeDescriptor + ', '
+      })
+      srcsetString.replace(/,$/, '')
+      return srcsetString
     },
     getWorkDetailsHref(work) {
       let path = '/'
       if (work.collection) {
-        path += work.collection.id
+        path += work.collection.id + '/'
       }
       path += work.id
       return path
+    },
+    getWorkTitlesLayerStyles(work) {
+      const bgColor = work.color || 'black'
+      return {
+        backgroundColor: bgColor,
+        color: tinycolor(bgColor).isLight() ? 'black' : 'white'
+      }
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-h3
-  font-family: 'Work Sans'
-  font-size: 2em
-  text-align center
-  font-weight normal
-  margin-top: 0
-
 ul
-  height: 300px
+  height: 600px
   display flex
   align-items center
 li
   height 100%
+  position relative
+  padding: 0
+  margin: 0
+
 img
   object-fit contain
   height: 100%
+
+li:hover
+  .titles-layer
+    opacity: 0.80
+    backdrop-filter blur(10px)
+    transform scale(1)
+
+.titles-layer
+  position absolute
+  background-color black
+  overflow hidden
+  display flex
+  flex-direction column
+  justify-content center
+  align-items center
+  opacity: 0
+  transform scale(0)
+  height 100%
+  width 100%
+  transition all 0.25s cubic-bezier(.7,.34,.25,1)
+  h3, h4, p
+    padding 0 2em
+    margin 0
+    opacity: 1
+    text-align center
+  p
+    text-align left
+    max-height 50%
+    overflow hidden
+    text-overflow ellipsis
+  .name
+    font-size 5em
+    font-family Work Sans
+    line-height: 0.8
+  .collection
+    font-size: 2em
+    text-transform uppercase
+    opacity: 0.75
+    font-weight normal
 
 .description
   a
@@ -73,30 +137,4 @@ img
     border-bottom 1px solid black
   a:hover
     border 1px solid black
-
-.--pig
-  width: 100%
-
-.--pig .pig-figure
-  width: 100% !important
-  height: auto !important
-  margin-bottom: gallery-gaps
-  background-color: white !important
-
-  img.pig-loaded:not(.pig-thumbnail)
-    height auto
-    transition: all 0.25s ease !important
-    box-sizing: border-box
-    // border: 1px solid black
-
-.pig-figure:hover
-  overflow: visible !important
-
-  img.pig-loaded:not(.pig-thumbnail)
-    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.25)
-    transform: translateY(-10px)
-    position: absolute
-
-  img.pig-thumbnail
-    display: none
 </style>
