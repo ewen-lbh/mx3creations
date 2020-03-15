b23b3dec233744e76727c4814ea262b3f2da5f00
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-15 16:19:11 +0100

    🚧 Start <Collection>

---
+++ components/Collection.vue
@@ -0,0 +1,50 @@
+<template lang="pug">
+main
+  h1 {{ name }}
+  section.description(v-html="description")
+  h2 Projets dans cette collection
+  Gallery(:works="ofCollection(id)")
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Gallery from '~/components/Gallery.vue'
+
+export default {
+  components: { Gallery },
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      default: null
+    },
+    id: {
+      type: String,
+      required: true
+    },
+    links: {
+      type: Object,
+      default: () => []
+    }
+  },
+  computed: {
+    ...mapGetters('works', ['ofCollection'])
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+h1
+  font-size 13vmin
+  line-height: 0.8
+  font-family Work Sans
+
+h2
+  font-size 3em
+  line-height: 0.8
+  font-family Work Sans
+  text-align center
+</style>
