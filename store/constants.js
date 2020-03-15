44c8f1c769951ec04a5c7a5f15e3afd310dab377
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-15 20:06:47 +0100

    🍱 Add missing constants store module

---
+++ store/constants.js
@@ -0,0 +1,32 @@
+export const state = () => ({
+  usingHrefs: {
+    illustrator: 'https://www.adobe.com/fr/products/illustrator.html',
+    photoshop: 'https://www.adobe.com/fr/products/photoshop.html',
+    gimp: 'https://www.gimp.org/',
+    assembly:
+      'https://apps.apple.com/us/app/assembly-graphic-design-art/id1024210402',
+    latex: 'https://www.latex-project.org/',
+    indesign: 'https://www.adobe.com/fr/products/indesign.html',
+    'after effects': 'https://www.adobe.com/fr/products/aftereffects.html',
+    nuxt: 'https://nuxtjs.org',
+    vue: 'https://vuejs.org',
+    javascript: 'https://developer.mozilla.org/fr/docs/Web/JavaScript',
+    pug: 'https://pugjs.org',
+    stylus: 'https://stylus-lang.com/',
+    html: 'https://developer.mozilla.org/fr/docs/Web/HTML',
+    sass: 'https://sass-lang.com/',
+    css: 'https://developer.mozilla.org/fr/docs/Web/CSS',
+    python: 'https://www.python.org',
+    django: 'https://www.djangoproject.com',
+    'django-rest-framework': 'https://www.django-rest-framework.org',
+    nginx: 'https://www.nginx.com/',
+    ovh: 'https://ovh.com',
+    postgresql: 'https://www.postgresql.org/',
+    plantuml: 'https://plantuml.com/',
+    coffeescript: 'https://coffeescript.org/',
+    lark: 'https://github.com/lark-parser/lark',
+    pychemin: 'https://ewen.works/pychemin',
+    typescript: 'https://www.typescriptlang.org/',
+    oclif: 'https://oclif.io/'
+  }
+})
