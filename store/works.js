73c55ba5208d274bc4597f80406bd0488daa7450
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-16 15:38:47 +0100

    🚀 Always pull works.json from static server

--- store/works.js
+++ store/works.js
@@ -39,17 +39,11 @@ export const actions = {
   async load({ commit, state }, { force } = { force: false }) {
     if (!force && state.loaded) return
     try {
-      let works
-      if (process.env.NODE_ENV === 'production') {
-        const { data } = await this.$axios.get(
-          'http://static.mx3creations.com/works.json'
-        )
-        works = data
-      } else {
-        works = require('@/static/works.json')
-      }
-      if (works) {
-        commit('SET', works)
+      const { data } = await this.$axios.get(
+        'http://static.mx3creations.com/works.json'
+      )
+      if (data) {
+        commit('SET', data)
         commit('POSTLOAD')
       }
     } catch (error) {}
