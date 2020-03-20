adf08ba1800c1972ff1497b62478391d9ae03740
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-20 13:59:49 +0100

    ✨ Create /credits

---
+++ pages/credits.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+main
+  h1 Crédits
+  h2 Icônes
+  dl
+    dt Icônes de réseau sociaux
+    dd Icons made by #[a(href="https://www.flaticon.com/authors/freepik" title="Freepik") Freepik] from #[a(href="https://www.flaticon.com/" title="Flaticon") www.flaticon.com]
+  h2 Polices
+  dl
+    dt Titres
+    dd(style="font-family: Work Sans") Work Sans
+    
+    dt Corps
+    dd(style="font-family: Inconsolata") Inconsolata
+</template>
