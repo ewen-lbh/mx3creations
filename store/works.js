a699e7a9ba9508fdacbb643c099f251ebdc5f774
ewen-lbh <ewen.lebihan7@gmail.com>
2020-03-25 23:18:00 +0100

    🚀 Revert 74adc7769a98a8d0d211ac8b44f3c63470ebc8fc

--- store/works.js
+++ store/works.js
@@ -41,17 +41,11 @@ export const actions = {
   async load({ commit, state }, { force } = { force: false }) {
     if (!force && state.loaded) return
     try {
-      let works
-      if (process.env.NODE_ENV === 'production') {
-        const { data } = await this.$axios.get(
-          'https://static.ewen.works/works.json'
-        )
-        works = data
-      } else {
-        works = require('@/static/works.json')
-      }
-      if (works) {
-        commit('SET', works)
+      const { data } = await this.$axios.get(
+        'https://static.ewen.works/works.json'
+      )
+      if (data) {
+        commit('SET', data)
         commit('POSTLOAD')
       }
     } catch (error) {
