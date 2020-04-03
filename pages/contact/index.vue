b6953bf37247191cc3171a75c6d52f26ec3e07db
ewen-lbh <ewen.lebihan7@gmail.com>
2020-04-03 12:05:13 +0200

    🚑 /contact: Fallback cuz the stupid netlify form won't work

---
+++ pages/contact/index.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+main
+  h1 Me contacter
+  dl
+    dt Par mail
+    dd: a(href="mailto:ewen.lebihan7@gmail.com") ewen.lebihan7@gmail.com
+    
+    dt Par twitter
+    dd Envoyez un message privé à #[a(href="httpss://ewen.works/to/twitter") @ewen_lbh]
+</template>
+
+<style lang="stylus" scoped>
+dl
+  max-width 500px
+  margin 0 auto
+  font-size: 1.2em
+  dt
+    font-size: 1.3em
+    margin-top 1em
+</style>
