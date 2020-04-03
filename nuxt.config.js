52d1a3f429425049e2ee66539ac8a95c4520169a
ewen-lbh <ewen.lebihan7@gmail.com>
2020-04-03 11:06:22 +0200

    🚀 Generate /contact?sent

--- nuxt.config.js
+++ nuxt.config.js
@@ -129,7 +129,8 @@ export default {
                 ...redirectsRoutes,
                 ...workAndCollectionsRoutes,
                 ...madeWithRoutes,
-                ...taggedRoutes
+                ...taggedRoutes,
+                '/contact?sent'
               ]
             })
         })
