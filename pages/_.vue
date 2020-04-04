123b342fc54116ceb441b878e63eb7165b501312
ewen-lbh <ewen.lebihan7@gmail.com>
2020-04-04 15:54:34 +0200

    ✨ Create /all

--- pages/_.vue
+++ pages/_.vue
@@ -4,20 +4,28 @@
     Work(v-bind="work")
   template(v-else-if="collection")
     Collection(v-bind="collection")
+  template(v-else-if="allWorks")
+    h1 Toutes mes créations
+    Gallery(:works="all")
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import Gallery from '~/components/Gallery.vue'
 import Work from '~/components/Work.vue'
 import Collection from '~/components/Collection.vue'
 
 export default {
-  components: { Work, Collection },
+  components: { Work, Collection, Gallery },
   asyncData({ error, route, store, app, redirect }) {
     const reqPath = route.params.pathMatch.split('/')
     const works = store.getters['works/all']
     const collections = store.getters['works/collections']
     const matchingCollection = collections.find((c) => c.id === reqPath[0])
     let matchingWork
+    if (reqPath[0] === 'all') {
+      return { allWorks: true }
+    }
     if (matchingCollection && reqPath.length > 1) {
       matchingWork = works.find(
         (w) => w.full_id === reqPath[0] + '/' + reqPath[1]
@@ -48,6 +56,7 @@ export default {
       error({ message: 'inexistantWork', statusCode: 404 })
     }
   },
+  computed: mapGetters('works', ['all']),
   head() {
     let title = ''
     if (this.collection) {
@@ -69,4 +78,8 @@ export default {
 <style lang="stylus" scoped>
 #page
   flex-grow 1
+h1
+  font-family Work Sans
+  font-size max(10vmin, 3rem)
+  margin-bottom .25em
 </style>
